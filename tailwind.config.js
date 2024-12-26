ea77423d7d98dec9efb6d45b6fe79bc6b4043c1c
Mochammad Yanuar Fitroni <71364199+MochammadYanuarFitroni@users.noreply.github.com>
2024-12-27 06:36:16 +0700

    update awidwaj

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      screens: {
+        "2xl": "1320px",
+      },
+    },
   },
   plugins: [],
 }
